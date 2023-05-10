b46888922016a4b985805997c77d4f86c8f29fd3
Ankit Arora <ankit.j.arora@gmail.com>
2023-05-10 12:37:54 +0530

    environment variables support while building

--- enums/parameters_enums/keys.go
+++ enums/parameters_enums/keys.go
@@ -8,39 +8,41 @@ RepoName
 type Key uint
 
 const (
-	RepoCloneUrl      Key = 1
-	RepoName          Key = 2
-	RepoBranch        Key = 3
-	RepoProviderToken Key = 4
-	RepoDirectoryPath Key = 5
-	RepoGitProvider   Key = 6
-	LoggerType        Key = 7
-	OrganizationID    Key = 8
-	DeploymentID      Key = 9
-	BuildID           Key = 10
-	EnvironmentID     Key = 11
-	RootDirectory     Key = 12
-	BuildCommand      Key = 13
-	PublishDirectory  Key = 14
-	NodeVersion       Key = 15
+	RepoCloneUrl         Key = 1
+	RepoName             Key = 2
+	RepoBranch           Key = 3
+	RepoProviderToken    Key = 4
+	RepoDirectoryPath    Key = 5
+	RepoGitProvider      Key = 6
+	LoggerType           Key = 7
+	OrganizationID       Key = 8
+	DeploymentID         Key = 9
+	BuildID              Key = 10
+	EnvironmentID        Key = 11
+	RootDirectory        Key = 12
+	BuildCommand         Key = 13
+	PublishDirectory     Key = 14
+	NodeVersion          Key = 15
+	EnvironmentVariables Key = 16
 )
 
 var keyToString = map[Key]string{
-	RepoCloneUrl:      "repo clone url",
-	RepoName:          "repo name",
-	RepoBranch:        "repo branch",
-	RepoProviderToken: "repo provider token",
-	RepoDirectoryPath: "repo directory path",
-	RepoGitProvider:   "repo git provider",
-	LoggerType:        "logger type",
-	OrganizationID:    "organization id",
-	DeploymentID:      "deployment id",
-	BuildID:           "build id",
-	EnvironmentID:     "environment id",
-	RootDirectory:     "root directory",
-	BuildCommand:      "build command",
-	PublishDirectory:  "publish directory",
-	NodeVersion:       "node version",
+	RepoCloneUrl:         "repo clone url",
+	RepoName:             "repo name",
+	RepoBranch:           "repo branch",
+	RepoProviderToken:    "repo provider token",
+	RepoDirectoryPath:    "repo directory path",
+	RepoGitProvider:      "repo git provider",
+	LoggerType:           "logger type",
+	OrganizationID:       "organization id",
+	DeploymentID:         "deployment id",
+	BuildID:              "build id",
+	EnvironmentID:        "environment id",
+	RootDirectory:        "root directory",
+	BuildCommand:         "build command",
+	PublishDirectory:     "publish directory",
+	NodeVersion:          "node version",
+	EnvironmentVariables: "environment variables",
 }
 
 func (k Key) String() string {
--- jobs/types.go
+++ jobs/types.go
@@ -3,6 +3,7 @@ package jobs
 import (
 	"fmt"
 	"github.com/deployment-io/deployment-runner-kit/enums/parameters_enums"
+	"strings"
 )
 
 type ContextV1 struct {
@@ -27,4 +28,24 @@ func GetParameterValue[T ParameterType](parameters map[parameters_enums.Key]inte
 		return v, fmt.Errorf("%s is not of valid type", k.String())
 	}
 	return v, nil
+}
+
+func EncodeEnvironmentVariablesToString(envVariables map[string]string) string {
+	envString := ""
+	for k, v := range envVariables {
+		entry := fmt.Sprintf("%s=%s", k, v)
+		envString += entry + "\n"
+	}
+	return envString
+}
+
+func DecodeEnvironmentVariablesToSlice(envVariables string) []string {
+	envString := strings.TrimSpace(envVariables)
+	envVariablesInSlice := strings.Split(envString, "\n")
+	var envVariablesSlice []string
+	for _, evIn := range envVariablesInSlice {
+		ev := strings.TrimSpace(evIn)
+		envVariablesSlice = append(envVariablesSlice, ev)
+	}
+	return envVariablesSlice
 }
