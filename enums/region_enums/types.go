1c09adcc7c279dabc366968feb16f11aa7435f69
Ankit Arora <ankit.j.arora@gmail.com>
2023-07-10 21:51:01 +0530

    added more AWS regions

--- enums/region_enums/types.go
+++ enums/region_enums/types.go
@@ -5,47 +5,62 @@ type Type uint
 const (
 	EuWest1 Type = iota + 1
 	ApSouth1
+	ApNorthEast1
+	ApNorthEast2
+	ApNorthEast3
+	ApSouthEast1
+	ApSouthEast2
+	CaCentral1
+	EuCentral1
+	EuNorth1
+	EuWest2
+	EuWest3
+	SaEast1
+	UsEast1
+	UsEast2
+	UsWest1
+	UsWest2
+	AfSouth1
+	ApEast1
+	ApSouth2
+	ApSouthEast3
+	ApSouthEast4
+	EuCentral2
+	EuSouth1
+	EuSouth2
+	MeCentral1
+	MeSouth1
 	MaxType //always add new types before MaxType
 )
 
-//ap-northeast-1:
-//AMI: ami-0a8be33c358a98829
-//ap-northeast-2:
-//AMI: ami-04ea56ea021b86658
-//ap-northeast-3:
-//AMI: ami-098436f864fd2d3b0
-//ap-south-1:
-//AMI: ami-0fbb5026d37f5f347
-//ap-southeast-1:
-//AMI: ami-0108ebda02138676e
-//ap-southeast-2:
-//AMI: ami-09a2afe68765e6248
-//ca-central-1:
-//AMI: ami-04ecbc5a516929022
-//eu-central-1:
-//AMI: ami-01ad80fd556799f44
-//eu-north-1:
-//AMI: ami-003e63396fb3cf41f
-//eu-west-1:
-//AMI: ami-0cbbbe6104042e885
-//eu-west-2:
-//AMI: ami-035d9fe64fff63b27
-//eu-west-3:
-//AMI: ami-050cc42ff95d4ed9d
-//sa-east-1:
-//AMI: ami-0f68db8769ddddc2e
-//us-east-1:
-//AMI: ami-03a8890aa01a8eaff
-//us-east-2:
-//AMI: ami-0280e3450a5a28e52
-//us-west-1:
-//AMI: ami-0057dd7b23e1c372e
-//us-west-2:
-//AMI: ami-036aba10f939dbcbb
-
 var TypeToString = map[Type]string{
-	EuWest1:  "eu-west-1",
-	ApSouth1: "ap-south-1",
+	EuWest1:      "eu-west-1",
+	ApSouth1:     "ap-south-1",
+	ApNorthEast1: "ap-northeast-1",
+	ApNorthEast2: "ap-northeast-2",
+	ApNorthEast3: "ap-northeast-3",
+	ApSouthEast1: "ap-southeast-1",
+	ApSouthEast2: "ap-southeast-2",
+	CaCentral1:   "ca-central-1",
+	EuCentral1:   "eu-central-1",
+	EuNorth1:     "eu-north-1",
+	EuWest2:      "eu-west-2",
+	EuWest3:      "eu-west-3",
+	SaEast1:      "sa-east-1",
+	UsEast1:      "us-east-1",
+	UsEast2:      "us-east-2",
+	UsWest1:      "us-west-1",
+	UsWest2:      "us-west-2",
+	AfSouth1:     "af-south-1",
+	ApEast1:      "ap-east-1",
+	ApSouth2:     "ap-south-2",
+	ApSouthEast3: "ap-southeast-3",
+	ApSouthEast4: "ap-southeast-4",
+	EuCentral2:   "eu-central-2",
+	EuSouth1:     "eu-south-1",
+	EuSouth2:     "eu-south-2",
+	MeCentral1:   "me-central-1",
+	MeSouth1:     "me-south-1",
 }
 
 func (a Type) String() string {
