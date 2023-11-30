64ef5f10e55b0880ae4b6ba999d27a1e20f80905
Ankit Arora <ankit.j.arora@gmail.com>
2023-11-30 14:38:16 +0530

    feat(previews): send PR comment 1. PR comment when preview starts 2. PR comment when preview builds successfuly

--- enums/notification_enums/types.go
+++ enums/notification_enums/types.go
@@ -3,6 +3,8 @@ package notification_enums
 type Type uint
 
 const (
-	BuildStarted   Type = 1
-	PreviewStarted Type = 2
+	BuildStarted            Type = 1
+	PreviewStarted          Type = 2
+	PreviewCompletedSuccess Type = 3
+	PreviewCompletedError   Type = 4
 )
