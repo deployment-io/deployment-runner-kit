cba9896f138c416d69cc1a0cf3c07d0a15f7405e
Ankit Arora <ankit.j.arora@gmail.com>
2023-12-04 12:27:22 +0530

    feat(previews): preview creation, deletion, and recreation
    1. Create preview build on PR open
    2. Create preview build on PR update/code commit
    3. Delete preview after 30 minutes on PR close/merge
    4. Abort PR deletion on PR reopen

--- enums/notification_enums/types.go
+++ enums/notification_enums/types.go
@@ -7,4 +7,7 @@ const (
 	PreviewStarted          Type = 2
 	PreviewCompletedSuccess Type = 3
 	PreviewCompletedError   Type = 4
+	PreviewDeletedSuccess   Type = 5
+	PreviewDeletionStarted  Type = 6
+	PreviewDeletionUndone   Type = 7
 )
