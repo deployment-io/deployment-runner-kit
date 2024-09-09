c7cddd25c54245ed6c7156117872d118df24b416
Ankit Arora <ankit.j.arora@gmail.com>
2024-09-09 14:11:35 +0530

    Add label field to region responses
    Updated region enums, controllers, and TypeScript types to include a 'label' field for region responses. This enhances the display and selection of regions in the dashboard UI components.

--- enums/region_enums/types.go
+++ enums/region_enums/types.go
@@ -69,6 +69,40 @@ func (a Type) String() string {
 	return TypeToString[a]
 }
 
+var TypeToName = map[Type]string{
+	EuWest1:      "Europe(Ireland) eu-west-1",
+	ApSouth1:     "Asia Pacific(Mumbai) ap-south-1",
+	ApNorthEast1: "Asia Pacific(Tokyo) ap-northeast-1",
+	ApNorthEast2: "Asia Pacific(Seoul) ap-northeast-2",
+	ApNorthEast3: "Asia Pacific(Osaka) ap-northeast-3",
+	ApSouthEast1: "Asia Pacific(Singapore) ap-southeast-1",
+	ApSouthEast2: "Asia Pacific(Sydney) ap-southeast-2",
+	CaCentral1:   "Canada(Central) ca-central-1",
+	EuCentral1:   "Europe(Frankfurt) eu-central-1",
+	EuNorth1:     "Europe(Stockholm) eu-north-1",
+	EuWest2:      "Europe(London) eu-west-2",
+	EuWest3:      "Europe(Paris) eu-west-3",
+	SaEast1:      "South America(Sao Paulo) sa-east-1",
+	UsEast1:      "US East(N. Virginia) us-east-1",
+	UsEast2:      "US East(Ohio) us-east-2",
+	UsWest1:      "US West(N. California) us-west-1",
+	UsWest2:      "US West(Oregon) us-west-2",
+	AfSouth1:     "Africa(Cape Town) af-south-1",
+	ApEast1:      "Asia Pacific(Hong Kong) ap-east-1",
+	ApSouth2:     "Asia Pacific(Hyderabad) ap-south-2",
+	ApSouthEast3: "Asia Pacific(Jakarta) ap-southeast-3",
+	ApSouthEast4: "Asia Pacific(Melbourne) ap-southeast-4",
+	EuCentral2:   "Europe(Zurich) eu-central-2",
+	EuSouth1:     "Europe(Milan) eu-south-1",
+	EuSouth2:     "Asia Pacific(Spain) eu-south-2",
+	MeCentral1:   "Middle East(UAE) me-central-1",
+	MeSouth1:     "Middle East(Bahrain) me-south-1",
+}
+
+func (a Type) Name() string {
+	return TypeToName[a]
+}
+
 var StringToType = map[string]Type{
 	"eu-west-1":      EuWest1,
 	"ap-south-1":     ApSouth1,
